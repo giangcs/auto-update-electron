5a01147622492ae588b76b93037914de568ac75a
sgiang1610 <songiang1610@gmail.com>
2025-02-25 17:23:05 +0700

    ipcMain test not work

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, ipcMain} = require("electron");
 const path = require("path");
 const { autoUpdater } = require("electron-updater");
-
-const log = require("electron-log");
-
-log.transports.file.resolvePath = () => path.join('C:/Users/63200557/Documents/auto-update-electron', 'logs/main.log');
-
-log.info('Hello, log');
-log.warn('Some problem appears')
-
 let win;
 function createWindow() {
     win =  new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
     })
-
+    win.webContents.openDevTools()
     win.loadFile(path.join(__dirname, "index.html"))
 }
 
@@ -26,33 +19,13 @@ function createWindow() {
 app.on("ready", () => {
   createWindow()
 });
-
-app.on('ready', ()=>{
-    createWindow()
-    autoUpdater.checkForUpdatesAndNotify()
+app.on('activate', () => {
+    if(BrowserWindow.getAllWindows().length === 0){
+        createWindow()
+    }
 })
 
-autoUpdater.on("update-available", () => {
-    log.info("Update available")
-});
-autoUpdater.on("update-not-available", () => {
-    log.info("Update not available")
-});
-
-autoUpdater.on("checking-for-update", () => {
-    log.info("Checking for update")
-});
-autoUpdater.on("download-progress", () => {
-    log.info("Download Progress")
-});
-autoUpdater.on("update-downloaded", () => {
-    log.info("Update Downloaded")
-});
-autoUpdater.on("error", (err) => {
-    log.info("Error in auto-updater" + err)
-});
-
-autoUpdater.on("download-progress", (progressTrack) => {
-    log.info("\n\ndownload-progress")
-    log.info(progressTrack)
+ipcMain.on('hello', async (e, arg) => {
+    console.log(arg);
+    e.reply('replay', 'this is frpom server side')
 })
